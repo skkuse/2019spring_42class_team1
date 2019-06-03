63b871e6dbb82ff6f7801f6bcd9af6d58a7a65a9
YongSeong Kim <kys911015@gmail.com>
2019-06-04 01:27:57 +0900

    extract frames and detect scenes

--- skk/seonbi/models.py
+++ skk/seonbi/models.py
@@ -2,18 +2,24 @@ from django.db import models
 
 
 class Video(models.Model):
+    class Status:
+        UPLOADED = 'UPLOADED'
+        DETECTING = 'DETECTING'
+        DETECTED = 'DETECTED'
     id = models.AutoField(primary_key=True)
+    status = models.CharField(max_length=20)
     filename = models.TextField()
     url = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
 
-class Censor(models.Model):
+class DetectedScene(models.Model):
+    class DetectionCause:
+        NUDITY = 'NUDITY'
+        SWEAR = 'SWEAR'    # 욕설
     id = models.AutoField(primary_key=True)
-    # 검열이 필욜한 장면과 검열된 부분의 시간이 다를 수 있다.
-    scene_from = models.BigIntegerField
-    scene_to = models.BigIntegerField
-    censor_from = models.BigIntegerField
-    censor_to = models.BigIntegerField
     src_video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    start_millis = models.BigIntegerField()
+    end_millis = models.BigIntegerField()
     cause = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
--- skk/seonbi/views.py
+++ skk/seonbi/views.py
@@ -11,7 +11,8 @@ import os
 import uuid
 from seonbi.utils import current_millis, extract_video_info, extract_frames
 import datetime
-from .models import Video
+from .models import Video, DetectedScene
+from nudenet import NudeClassifier
 
 
 bucket_name = 'seonbi'
@@ -25,33 +26,70 @@ def index(request):
             name = str(uuid.uuid4()) + '_' + str(current_millis())
             filename = name + ext
             filepath = default_storage.save(os.path.join(settings.MEDIA_ROOT, filename), request.FILES['file'])
-            vinfo = extract_video_info(filepath)
+            # vinfo = extract_video_info(filepath)
 
-            # upload gcp
-            client = storage.Client.from_service_account_json(settings.GCP_KEY_PATH)
-            bucket = client.get_bucket(bucket_name)
             now = datetime.datetime.now()
-            gcp_filepath = os.path.join('%d-%d-%d' % (now.year, now.month, now.day), filename)
-            blob = Blob(gcp_filepath, bucket)
-            blob.upload_from_filename(filepath)
-            blob.make_public()
-            url = gcp_url_format % (bucket_name, gcp_filepath)
+            gcp_path = os.path.join('%d-%d-%d' % (now.year, now.month, now.day), filename)
+            url = upload_to_gcp(filepath, gcp_path)
 
-            # TODO: extract frames and make censors using celery
-            output_dir = os.path.join(os.path.dirname(filepath), name)
-            os.makedirs(output_dir)
-            extract_frames(filepath, output_dir, frame_size=(16 * 10, 9 * 10), interval_millis=10000)
-
-
-            from nudenet import NudeClassifier
-            classifier = NudeClassifier(settings.NUDE_NET_CLASSIFIER_MODEL_PATH)
-            censors = dict()
-            for frame_img in os.listdir(output_dir):
-                frame_img_path = os.path.join(output_dir, frame_img)
-                if not os.path.exists(frame_img_path) or os.path.isdir(frame_img_path):
-                    continue
-                nudity_prob = classifier.classify(frame_img_path).get(frame_img_path).get('nude')
-                censors[frame_img] = str(nudity_prob)
-            print(censors)
-            return JsonResponse({'url': url, 'censors': censors})
+            video = Video(status=Video.Status.UPLOADED, filename=uploaded.name, url=url)
+            video.save()
+            detected = detect(filepath, video)
+            return JsonResponse({'url': url, 'scenes': list(map(lambda s: {'start_millis': s.start_millis, 'end_millis': s.end_millis}, detected))})
     return render(request, 'index.html', {})
+
+
+def upload_to_gcp(src_path, gcp_path):
+    client = storage.Client.from_service_account_json(settings.GCP_KEY_PATH)
+    bucket = client.get_bucket(bucket_name)
+    blob = Blob(gcp_path, bucket)
+    blob.upload_from_filename(src_path)
+    blob.make_public()
+    return gcp_url_format % (bucket_name, gcp_path)
+
+
+def frame_order(str):
+    try:
+        return int(os.path.splitext(str)[0].split('_')[1])
+    except:
+        return -1
+
+
+def detect(src_path, video):
+    frame_size = (160, 90)
+    interval_millis = 1000
+    classifier = NudeClassifier(settings.NUDE_NET_CLASSIFIER_MODEL_PATH)
+
+    video.status = Video.Status.DETECTING
+
+    src_name = os.path.splitext(os.path.basename(src_path))[0]
+    output_dir = os.path.join(os.path.dirname(src_path), src_name)
+    os.makedirs(output_dir)
+    extract_frames(src_path, output_dir, frame_size, interval_millis)
+
+    detected = list()
+    thresold = 5 * interval_millis
+    for frame in sorted(os.listdir(output_dir), key=lambda f: frame_order(f)):
+        order = frame_order(frame)
+        if order < 0:
+            continue
+        framepath = os.path.join(output_dir, frame) 
+        if not os.path.exists(framepath) or os.path.isdir(framepath):
+            continue
+        result = classifier.classify(framepath)[framepath]
+        nudity_prob = result['nude']
+        if nudity_prob > 0.8:
+            start_millis = order * interval_millis
+            end_millis = (order + 1) * interval_millis
+            if not detected:
+                detected.append(DetectedScene(src_video=video, start_millis=start_millis, end_millis=1000, cause=DetectedScene.DetectionCause.NUDITY))
+            else:
+                latest = detected[-1]
+                if latest.end_millis + thresold < latest.start_millis:
+                    detected.append(DetectedScene(src_video=video, start_millis=start_millis, end_millis=1000, cause=DetectedScene.DetectionCause.NUDITY))
+                else:
+                    latest.end_millis = end_millis
+    for scene in detected:
+        scene.save()
+    video.status = Video.Status.DETECTED
+    return detected
